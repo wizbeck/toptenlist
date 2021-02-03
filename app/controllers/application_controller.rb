514d56e56fad16c829e178aa8af9a75cc209ccf5
Wesley <wes.beck21@outlook.com>
2021-02-02 20:04:35 -0800

    layout issues, minor adds to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-
-
+#helpers give access to methods in views
+helper_method :current_user, :logged_in?
   
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
